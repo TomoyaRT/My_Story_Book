b0628c3d01eb293610f364859b1bfab920df42c7
Roger <rogeryen@damaiapp.com.tw>
2022-11-24 11:57:14 +0800

    feat: add story book and deploy sh file

---
+++ Note.md
@@ -0,0 +1,12 @@
+### Command (Mac Terminal)
+1. cd desktop
+2. yarn create vite
+3. Project name: my-story-book
+4. Select a framework: React
+5. Select a variant: TypeScript
+6. yarn
+7. yarn dev
+8. npx storybook init
+9. yarn add --dev @storybook/addons @storybook/theming
+
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  publicDir: '../public', // rewrite public folder path
+  root: 'src', // Project root directory (where index.html is located)
+  base: 'My_Story_Book', // Base public path when served in development or production.
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3001
+  },
+  resolve: {
+    alias: { // configuring resolve to alias for src folder path
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
